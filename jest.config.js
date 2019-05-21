f1d8f68736775dd001197fa0d42602d31444ca86
Patrick Assoa <kanian77@gmail.com>
2019-05-21 11:55:27 +0000

    chore: initialization

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(js|jsx)$',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ["/node_modules/"],
+};
